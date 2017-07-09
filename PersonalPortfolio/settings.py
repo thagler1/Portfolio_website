1112b85938af79b26e3cd4398321aa5da27eff13
thagler1 <tdhagler1@gmail.com>
2017-07-09 12:36:06 -0500

    Added analytics

--- PersonalPortfolio/settings.py
+++ PersonalPortfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = settings_secret.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
