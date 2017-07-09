ef9d85cd3b932eec25ad5da82d6282b3cf3f6d9e
thagler1 <tdhagler1@gmail.com>
2017-07-09 12:53:36 -0500

    removed allowed_hosts

--- PersonalPortfolio/settings.py
+++ PersonalPortfolio/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = settings_secret.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS = settings_secret.ALLOWED_HOSTS
 
 
 # Application definition
