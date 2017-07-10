9d6665d38544bd0257519cd9038685966e35021b
thagler1 <tdhagler1@gmail.com>
2017-07-10 12:20:37 -0500

    changed base URL

--- PersonalPortfolio/urls.py
+++ PersonalPortfolio/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^home', views.index),
+    url(r'^', views.index),
     url(r'^receive_contact',views.recieve_contact ),
     url(r'^admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
